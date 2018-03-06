0f7c30f1dec7c263e08d3c2b81cbd4400e86bd41
Simon Merkt <simon.merkt@epfl.ch>
2018-03-06 16:46:18 +0100

    cc

--- testVecteur.cc
+++ testVecteur.cc
@@ -5,7 +5,7 @@ using namespace std;
 
 int main(){
 	Vecteur v;
-	v.augmente(2.0); v.augmente(3.0); 
+	v.augmente(2.0); v.augmente(3.0);
 	v.affiche();
 	v.set_coord(2,4.0); v.augmente(6.0);
 	v.affiche();
@@ -18,6 +18,8 @@ int main(){
 	cout << v.taille() << " " << v.get_coord(2) <<  endl;
 	Vecteur v2;
 	v2.augmente(2.0); v2.augmente(4.0);
-	if(v.compare(v2)){cout << "Yes" << endl;}else{cout << "Non" << endl;} 
+	if(v.compare(v2)){cout << "Yes" << endl;}else{cout << "Non" << endl;}
 	return 0;
 }
+
+// cest trop cool
