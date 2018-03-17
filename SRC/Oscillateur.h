6696003198c6103f865cbab8c1a1fcee0c4e1406
Timothee Dao <timothee.dao@epfl.ch>
2018-03-17 14:48:32 +0100

    constructeur class Oscillateur et opertor<<

--- SRC/Oscillateur.h
+++ SRC/Oscillateur.h
@@ -1,12 +1,21 @@
 #pragma once
+#include <initializer_list>
 #include "Vecteur.h"
+
 
 class Oscillateur{
 public:
-  Oscillateur();
+  explicit Oscillateur(const unsigned int& n=3);
+  explicit Oscillateur(const double& Px, const double& Py, const double& Pz,
+                       const double& Qx, const double& Qy, const double& Qz);
+  explicit Oscillateur(const std::initializer_list<double>& liP,
+                       const std::initializer_list<double>& liQ);
+
   Vecteur f(const double& t) const;
-  void affiche();
+  std::ostream& affiche(std::ostream& sortie=std::cout) const; // permet permet d'afficher le vecteur par composants sur un flot de sortie
 private:
   Vecteur P;
   Vecteur Q;
 };
+
+std::ostream& operator<<(std::ostream& sortie, const Oscillateur& osc);
--- SRC/testOscillateur.cc
+++ SRC/testOscillateur.cc
@@ -4,7 +4,22 @@
 using namespace std;
 
 int main(){
-  Oscillateur o;
-  o.affiche();
+  try{
+  Oscillateur osc1;
+  Oscillateur osc2(5);
+  Oscillateur osc3(1.1,2.2,3.3,4.4,5.5,6.6);
+  cout << osc1 << osc2 << osc3 << endl;
+  Oscillateur osc4({1,2},{3,4});
+  cout << osc4 << endl;
+  Oscillateur osc5({1,2},{3,4,5});
+  cout << osc5 << endl;
+  }catch(Erreur err){
+    cerr << endl << endl << "### ERREUR FATALE ###" << endl;
+		cerr << "Type d'erreur : " << err.type << endl;
+    cerr << "Dans fichier  : testOscillateur.cc" << endl;
+		cerr << "Dans fonction : " << err.fct << endl;
+		cerr << "Description   : " << err.description << endl;
+		cerr << "### FIN DU PROGRAMME ###" << endl << endl;
+  }
   return 0;
 }
