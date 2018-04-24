30a743b4654a0445dbdff46834ac860bd9125e91
Timothee Dao <timothee.dao@epfl.ch>
2018-04-24 15:09:22 +0200

    petite maj

--- SRC/testPendule.cc
+++ SRC/testPendule.cc
@@ -6,16 +6,15 @@
 using namespace std;
 
 int main(){
-Pendule p({M_PI/2}
-          ,{0.0}
-          ,{0.0}
-          ,{0.0}
-          ,1.0, 1.0, 0.0);
-
-IntegrateurEulerCromer euler(&p, 0.1, 0);
-for(int i(1);i<=100;i++){
-  euler.evolue();
-  cout << euler << endl;
-}
+  try{
+    Pendule p({M_PI/2},{0.0},{0.0},{0.0},1.0, 1.0, 0.0);
+    IntegrateurEulerCromer euler(&p, 0.1, 0);
+    for(int i(1);i<=100;i++){
+      euler.evolue();
+      cout << euler << endl;
+    }
+  }catch(Erreur err){
+    err.affiche("testPendule.cc");
+  }
   return 0;
 }
